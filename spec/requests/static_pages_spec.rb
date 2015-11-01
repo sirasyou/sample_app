2ba87758b8ec5d81c4bf3974956112c6d09a7289
sirasyou <s.shota.s@gmail.com>
2015-11-01 10:35:38 +0900

    Add a sttaicpage enshu

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
@@ -12,7 +14,7 @@ describe "StaticPages" do
 
     it "should have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
 
   end
@@ -26,7 +28,7 @@ describe "StaticPages" do
 
     it "should have the right title" do
       visit '/static_pages/help'
-      expect(page).to have_title("Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
 
   end
@@ -40,7 +42,21 @@ describe "StaticPages" do
 
     it "should have the right title" do
       visit '/static_pages/about'
-      expect(page).to have_title('About')
+      expect(page).to have_title("#{base_title} | About")
+    end
+
+  end
+
+  describe 'Contact page' do
+
+    it "should have the right title 'Contact page'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | contact")
+    end
+
+    it "should have the content 'Contact page'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content("contact page")
     end
 
   end
