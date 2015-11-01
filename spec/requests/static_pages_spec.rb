d2065968553bac6fb6a932de4a9bb12394d1302a
sirasyou <s.shota.s@gmail.com>
2015-11-01 10:10:48 +0900

    repair Gemfile.lock

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+
+  describe "Home page" do
+
+    it "should have the content 'Sample App'" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Home")
+    end
+
+  end
+
+  describe "Help page" do
+
+    it "should have the content 'Help page'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Help")
+    end
+
+  end
+
+  describe 'About page' do
+
+    it "should have the content 'About page'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/about'
+      expect(page).to have_title('About')
+    end
+
+  end
+
+end
